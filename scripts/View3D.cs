895b92bf593863511874a9cf3230a4e4a1c03411
Mubinul Haque <80097192+mubinulhaque@users.noreply.github.com>
2024-09-17 20:27:45 +0100

    Redo skeleton transform
    Skeleton transform are now done according to the Pure3D specification

--- scripts/View3D.cs
+++ scripts/View3D.cs
@@ -1,6 +1,7 @@
 using Godot;
 using System;
 using System.Collections.Generic;
+using System.Security.Cryptography.X509Certificates;
 
 public partial class View3D : SubViewport
 {
@@ -24,31 +25,31 @@ public partial class View3D : SubViewport
 
 		// Define a placeholder mesh
 		SphereMesh sphere = new SphereMesh();
-		sphere.Radius = 1f;
+		sphere.Radius = .2f;
 		sphere.Height = sphere.Radius * 2;
 
 		foreach (var child in bones.Children)
 		{
 			if (child is Pure3D.Chunks.SkeletonJoint)
 			{
-				var joint = (Pure3D.Chunks.SkeletonJoint)child;
+				Pure3D.Chunks.SkeletonJoint joint = (Pure3D.Chunks.SkeletonJoint)child;
 				skeleton.AddBone(joint.Name);
 				skeleton.EditorDescription += "Bone: " + joint.Name + "\n";
 
-				var restPose = joint.RestPose;
-				var boneTransform = new Transform3D(new Projection(
-					new Vector4(joint.RestPose.M21, joint.RestPose.M22, joint.RestPose.M23, joint.RestPose.M24),
-					new Vector4(joint.RestPose.M31, joint.RestPose.M32, joint.RestPose.M33, joint.RestPose.M34),
-					new Vector4(joint.RestPose.M41, joint.RestPose.M42, joint.RestPose.M43, joint.RestPose.M44),
-					new Vector4(joint.RestPose.M11, joint.RestPose.M12, joint.RestPose.M13, joint.RestPose.M14)
-				));
+				Pure3D.Matrix restPose = joint.RestPose;
+				 
+				Transform3D boneTransform = new Transform3D(new Basis(
+					new Vector3(restPose.M11, restPose.M12, restPose.M13),
+					new Vector3(restPose.M21, restPose.M22, restPose.M23),
+					new Vector3(restPose.M31, restPose.M32, restPose.M33)
+				), new Vector3(restPose.M41, restPose.M42, restPose.M43));
 				skeleton.SetBoneRest(boneIndex, boneTransform);
 
 				//Node3D boneIndicator = new Node3D();
 				MeshInstance3D boneIndicator = new MeshInstance3D();
 				boneIndicator.Name = joint.Name;
 				boneIndicator.Mesh = sphere;
-				boneIndicator.Position = boneTransform.Origin;
+				boneIndicator.Transform = boneTransform;
 				AddChild(boneIndicator);
 			} else
 			{
