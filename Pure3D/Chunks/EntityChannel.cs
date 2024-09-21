61ab78dd32e1c41c0031d97fd35b416e6d867dee
Mubinul Haque <80097192+mubinulhaque@users.noreply.github.com>
2024-09-21 12:24:52 +0100

    Add Entity Channel chunk
    Expose all properties viewable in Lucas Pure3D editor 4.5

---
+++ Pure3D/Chunks/EntityChannel.cs
@@ -0,0 +1,53 @@
+using System.IO;
+using System.Text;
+using Godot;
+
+namespace Pure3D.Chunks
+{
+    [ChunkType(1184007)]
+    public class EntityChannel : Chunk
+    {
+        public uint NumberOfFrames;
+        public string[] Values;
+        public uint Version;
+        public string Param;
+        public ushort[] Frames;
+
+        public EntityChannel(File file, uint type) : base(file, type)
+        {
+        }
+
+        public override void ReadHeader(Stream stream, long length)
+        {
+            BinaryReader reader = new BinaryReader(stream);
+            Version = reader.ReadUInt32();
+            Param = Util.ZeroTerminate(Encoding.ASCII.GetString(reader.ReadBytes(4)));
+            NumberOfFrames = reader.ReadUInt32();
+            Frames = new ushort[NumberOfFrames];
+
+            for (int i = 0; i < NumberOfFrames; i++)
+            {
+                Frames[i] = reader.ReadUInt16();
+            }
+
+            Values = new string[NumberOfFrames];
+            reader.ReadByte();
+            for (int i = 0; i < NumberOfFrames; i++)
+            {
+                Values[i] = Util.ZeroTerminate(Encoding.ASCII.GetString(reader.ReadBytes(17)));
+            }
+
+            reader.ReadBytes(19); // Unsure what the last few bytes are for
+        }
+
+        public override string ToString()
+        {
+            return $"{Param} Entity Channel Version: {Version}, Frames: {NumberOfFrames}";
+        }
+
+        public override string ToShortString()
+        {
+            return $"Entity Channel";
+        }
+    }
+}
